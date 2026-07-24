933a71a9b860391ca86cca6e977cec84bb8f6748
triapriyogibahari7-pixel <triapriyogibahari7@gmail.com>
2026-07-24 16:37:21 +0700

    fix(pwa): invalidate stale Nara and Studio shells for v14

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const VERSION = "ngeblogging-app-v12-20260724";
+const VERSION = "ngeblogging-app-v14-20260724";
 const SHELL_CACHE = `${VERSION}-shell`;
 const ASSET_CACHE = `${VERSION}-assets`;
 const APP_SHELL = ["/", "/site.webmanifest", "/favicon.svg"];
