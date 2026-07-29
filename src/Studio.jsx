e730f998be6d717172fd950305147cc5f499c417
triapriyogibahari7-pixel <triapriyogibahari7@gmail.com>
2026-07-29 22:51:27 +0700

    Activate Studio v148 interface authority

--- src/Studio.jsx
+++ src/Studio.jsx
@@ -3,11 +3,13 @@ import "./studio-style-authority-v144.js";
 import "./studio-device-mode-v140.js";
 import "./nara-size-authority-v144.js";
 import "./studio-shell-controller-v147.js";
+import "./studio-interface-v148.js";
 import "./studio-layout-v140.css";
 import "./studio-layout-hotfix-v141.css";
 import "./studio-layout-hotfix-v142.css";
 import "./studio-layout-authority-v144.css";
 import "./studio-layout-authority-v145.css";
 import "./studio-interface-authority-v147.css";
+import "./studio-interface-v148.css";
 
 export default StudioFastGate;
