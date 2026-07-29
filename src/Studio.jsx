934645ebd539714f44a0b169ac30fa064ff0a1b2
triapriyogibahari7-pixel <triapriyogibahari7@gmail.com>
2026-07-29 18:17:07 +0700

    Aktifkan authority layout tunggal dan ukuran Nara v144

--- src/Studio.jsx
+++ src/Studio.jsx
@@ -1,7 +1,10 @@
 import StudioFastGate from "./StudioFastGate.jsx";
+import "./studio-style-authority-v144.js";
 import "./studio-device-mode-v140.js";
+import "./nara-size-authority-v144.js";
 import "./studio-layout-v140.css";
 import "./studio-layout-hotfix-v141.css";
 import "./studio-layout-hotfix-v142.css";
+import "./studio-layout-authority-v144.css";
 
 export default StudioFastGate;
