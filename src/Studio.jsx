a195d08bd49d408dc2ceb4f25a28a12be7abc71f
triapriyogibahari7-pixel <triapriyogibahari7@gmail.com>
2026-07-29 17:11:47 +0700

    fix(studio): muat hotfix layout v141 terakhir

--- src/Studio.jsx
+++ src/Studio.jsx
@@ -1,5 +1,6 @@
 import StudioFastGate from "./StudioFastGate.jsx";
 import "./studio-device-mode-v140.js";
 import "./studio-layout-v140.css";
+import "./studio-layout-hotfix-v141.css";
 
 export default StudioFastGate;
