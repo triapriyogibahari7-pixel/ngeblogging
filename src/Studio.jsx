ccaaf9ebba46a4e6bd296cfdc4e833fc29f57307
triapriyogibahari7-pixel <triapriyogibahari7@gmail.com>
2026-07-31 21:18:47 +0700

    Muat authority mobile dan auth v180

--- src/Studio.jsx
+++ src/Studio.jsx
@@ -23,5 +23,6 @@ import "./studio-mobile-stability-v176.js";
 import "./studio-screenshot-stability-v177.js";
 import "./studio-finalization-v178.js";
 import "./studio-mobile-runtime-v179.js";
+import "./studio-mobile-runtime-v180.js";
 
 export default StudioFastGate;
