b02084a2ab84dfd85bc610dfd40336cc563a09f7
triapriyogibahari7-pixel <triapriyogibahari7@gmail.com>
2026-08-01 20:22:42 +0700

    fix(studio): load v194 Theme Studio and Nara authority last

--- src/Studio.jsx
+++ src/Studio.jsx
@@ -38,5 +38,6 @@ import "./studio-screenshot-recovery-v191.js";
 import "./studio-screenshot-recovery-v191-hotfix.css";
 import "./studio-screenshot-recovery-v193.js";
 import "./studio-screenshot-recovery-v193-hotfix.css";
+import "./studio-nara-theme-v194.js";
 
 export default StudioFastGate;
