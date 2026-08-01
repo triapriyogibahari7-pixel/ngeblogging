88d024e1f7c7113dbd1fe5c6391aca033664c15a
triapriyogibahari7-pixel <triapriyogibahari7@gmail.com>
2026-08-01 08:18:42 +0700

    test: isolate baseline build from v185 authority

--- src/Studio.jsx
+++ src/Studio.jsx
@@ -27,6 +27,5 @@ import "./studio-production-recovery-v180.js";
 import "./studio-mobile-hardening-v181.js";
 import "./studio-production-v183.js";
 import "./studio-production-v183-controls.css";
-import "./studio-mobile-authority-v185.js";
 
 export default StudioFastGate;
