ce4dc5dfd337fd9570d94b2cc776748b8b900aeb
triapriyogibahari7-pixel <triapriyogibahari7@gmail.com>
2026-08-04 14:53:32 +0700

    Load v266 production analytics and sidebar runtime

--- src/Studio.jsx
+++ src/Studio.jsx
@@ -107,5 +107,6 @@ import "./studio-shell-v265.css";
 import "./studio-shell-v265-final-hotfix.css";
 import "./studio-editor-navigation-v266.js";
 import "./studio-editor-navigation-v266.css";
+import "./studio-runtime-v266.js";
 
 export default StudioFastGate;
