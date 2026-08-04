119bb8b1f0be40c05b125d01c68903f7970fcb27
triapriyogibahari7-pixel <triapriyogibahari7@gmail.com>
2026-08-04 14:35:00 +0700

    Load screenshot authority v265 after Theme Studio v264

--- src/Studio.jsx
+++ src/Studio.jsx
@@ -99,5 +99,7 @@ import "./studio-shell-v263.css";
 import "./studio-shell-v263-hotfix.css";
 import "./studio-theme-layout-v264.js";
 import "./studio-theme-layout-v264.css";
+import "./studio-screenshot-authority-v265.js";
+import "./studio-screenshot-authority-v265.css";
 
 export default StudioFastGate;
