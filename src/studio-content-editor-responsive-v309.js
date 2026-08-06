d5a5f237ffabf95775c7b9ce12c8d08f03ec414d
triapriyogibahari7-pixel <triapriyogibahari7@gmail.com>
2026-08-06 08:34:31 +0700

    Studio v309: add Posts/Pages responsive runtime marker

---
+++ src/studio-content-editor-responsive-v309.js
@@ -0,0 +1,7 @@
+import "./studio-content-editor-responsive-v309.css";
+
+export const STUDIO_CONTENT_EDITOR_RELEASE_V309 = "studio-content-editor-responsive-v309-20260806";
+
+if (typeof document !== "undefined") {
+  document.documentElement.dataset.studioContentEditorV309 = STUDIO_CONTENT_EDITOR_RELEASE_V309;
+}
