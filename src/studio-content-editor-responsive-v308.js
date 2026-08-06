2f2142f13f4ff3b97c2e8e8331636c88d6c5b216
triapriyogibahari7-pixel <triapriyogibahari7@gmail.com>
2026-08-06 07:30:46 +0700

    Studio v308: load responsive editor authority

---
+++ src/studio-content-editor-responsive-v308.js
@@ -0,0 +1,7 @@
+import "./studio-content-editor-responsive-v308.css";
+
+export const STUDIO_CONTENT_EDITOR_RELEASE_V308 = "studio-content-editor-responsive-v308-20260806";
+
+if (typeof document !== "undefined") {
+  document.documentElement.dataset.studioContentEditorV308 = STUDIO_CONTENT_EDITOR_RELEASE_V308;
+}
