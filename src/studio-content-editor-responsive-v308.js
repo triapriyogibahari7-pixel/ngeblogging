33748725bd935718bbee772a455459c36f214732
triapriyogibahari7-pixel <triapriyogibahari7@gmail.com>
2026-08-06 12:51:38 +0700

    v318: load screenshot-driven Studio hotfix

--- src/studio-content-editor-responsive-v308.js
+++ src/studio-content-editor-responsive-v308.js
@@ -3,12 +3,14 @@ import "./studio-content-editor-post-page-polish-v309.css";
 import "./studio-content-editor-desktop-site-v310.css";
 import "./studio-content-editor-final-v316.js";
 import "./studio-final-v317.css";
+import "./studio-hotfix-v318.css";
 
 export const STUDIO_CONTENT_EDITOR_RELEASE_V308 = "studio-content-editor-responsive-v308-20260806";
 export const STUDIO_CONTENT_EDITOR_POLISH_RELEASE_V309 = "studio-content-editor-post-page-polish-v309-20260806";
 export const STUDIO_CONTENT_EDITOR_DESKTOP_SITE_RELEASE_V310 = "studio-content-editor-desktop-site-v310-20260806";
 export const STUDIO_CONTENT_EDITOR_FINAL_RELEASE_V316 = "studio-content-editor-final-v316-20260806";
 export const STUDIO_FINAL_RESPONSIVE_RELEASE_V317 = "studio-final-responsive-v317-20260806";
+export const STUDIO_SCREENSHOT_HOTFIX_RELEASE_V318 = "studio-screenshot-hotfix-v318-20260806";
 
 if (typeof document !== "undefined") {
   document.documentElement.dataset.studioContentEditorV308 = STUDIO_CONTENT_EDITOR_RELEASE_V308;
@@ -16,4 +18,5 @@ if (typeof document !== "undefined") {
   document.documentElement.dataset.studioContentEditorDesktopSiteV310 = STUDIO_CONTENT_EDITOR_DESKTOP_SITE_RELEASE_V310;
   document.documentElement.dataset.studioContentEditorFinalV316 = STUDIO_CONTENT_EDITOR_FINAL_RELEASE_V316;
   document.documentElement.dataset.studioFinalResponsiveV317 = STUDIO_FINAL_RESPONSIVE_RELEASE_V317;
+  document.documentElement.dataset.studioScreenshotHotfixV318 = STUDIO_SCREENSHOT_HOTFIX_RELEASE_V318;
 }
