ccc3fc50556832999a6043645d62d13888f54b9c
triapriyogibahari7-pixel <triapriyogibahari7@gmail.com>
2026-08-06 10:22:27 +0700

    chain v313 Posts Pages final editor authority

--- src/studio-content-editor-responsive-v308.js
+++ src/studio-content-editor-responsive-v308.js
@@ -1,13 +1,16 @@
 import "./studio-content-editor-responsive-v308.css";
 import "./studio-content-editor-post-page-polish-v309.css";
 import "./studio-content-editor-desktop-site-v310.css";
+import "./studio-content-editor-final-v313.js";
 
 export const STUDIO_CONTENT_EDITOR_RELEASE_V308 = "studio-content-editor-responsive-v308-20260806";
 export const STUDIO_CONTENT_EDITOR_POLISH_RELEASE_V309 = "studio-content-editor-post-page-polish-v309-20260806";
 export const STUDIO_CONTENT_EDITOR_DESKTOP_SITE_RELEASE_V310 = "studio-content-editor-desktop-site-v310-20260806";
+export const STUDIO_CONTENT_EDITOR_FINAL_RELEASE_V313 = "studio-content-editor-final-v313-20260806";
 
 if (typeof document !== "undefined") {
   document.documentElement.dataset.studioContentEditorV308 = STUDIO_CONTENT_EDITOR_RELEASE_V308;
   document.documentElement.dataset.studioContentEditorPolishV309 = STUDIO_CONTENT_EDITOR_POLISH_RELEASE_V309;
   document.documentElement.dataset.studioContentEditorDesktopSiteV310 = STUDIO_CONTENT_EDITOR_DESKTOP_SITE_RELEASE_V310;
+  document.documentElement.dataset.studioContentEditorFinalV313 = STUDIO_CONTENT_EDITOR_FINAL_RELEASE_V313;
 }
