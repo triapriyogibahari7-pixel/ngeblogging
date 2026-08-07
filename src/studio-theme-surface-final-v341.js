10b15e4545ec55f7d4051ac934dd5fd9eaaa0fca
triapriyogibahari7-pixel <triapriyogibahari7@gmail.com>
2026-08-07 11:28:05 +0700

    v341: load final Theme Studio surface authority

---
+++ src/studio-theme-surface-final-v341.js
@@ -0,0 +1,7 @@
+import "./studio-theme-surface-final-v341.css";
+
+export const STUDIO_THEME_SURFACE_FINAL_RELEASE_V341 = "studio-theme-surface-final-v341-20260807";
+
+if (typeof document !== "undefined") {
+  document.documentElement.dataset.studioThemeSurfaceFinalV341 = STUDIO_THEME_SURFACE_FINAL_RELEASE_V341;
+}
